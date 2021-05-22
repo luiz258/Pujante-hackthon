e3218da1f684afc7c04101c425fa2c730ce8b362
LUIZ MARCELO BARBOSA <luizmarcelo258@gmail.com>
2021-05-22 02:18:32 -0300

    inicio

--- front/src/Pages/Narrativa/Empatia/EmpatiaIntroducao/Inicio/index.js
+++ front/src/Pages/Narrativa/Empatia/EmpatiaIntroducao/Inicio/index.js
@@ -3,14 +3,35 @@ import React from "react";
 import Box from '@material-ui/core/Box';
 import Navbar from "../../../../../components/Navbar";
 import '../Inicio/index.css';
+import { Typography } from '@material-ui/core';
 
-function Inicio(){
-    return (
-    
-     <Box className="fundo">
-         <Navbar></Navbar>
-         <div>ssdsad</div>
-     </Box>
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import Fab from '@material-ui/core/Fab';
+import { positions } from '@material-ui/system';
+
+function Inicio() {
+    return (<>
+        <Navbar></Navbar>
+        <Box >
+            <div className="fundo">
+                <div className="titulo">
+                    <Typography className="fontTitulo" variant="h3" >
+                        Seja bem vindo a realidade empreendedora!
+                    </Typography>
+                    <Typography className="descricao" variant="h4" component="h3">
+                        Vamos conhecer como as coisas funcionam nesse mundo paralelo!
+                    </Typography>
+                </div>
+
+                <Box position="relative"  left="60%">
+                    <Fab color="primary" aria-label="arrowRight" variant="extended" pos>
+                        Proximo <KeyboardArrowRightIcon />
+                    </Fab>
+                </Box>
+            </div>
+        </Box>
+    </>
     )
 }
 
