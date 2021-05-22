400b00de264e8b250acdf339f545eea5b889c38a
Flávio Manuel <flaviomanuel258@gmail.com>
2021-05-22 01:58:53 -0300

    initial Login page

--- front/src/Pages/LoginPage/index.js
+++ front/src/Pages/LoginPage/index.js
@@ -1,9 +1,26 @@
+import {TextField, FormControl, Container, makeStyles, Grid} from '@material-ui/core';
+
 
 const LoginPage = () => {
     return (
-        <div>
-            login page component
-        </div>
+    <Container>
+       <FormControl>
+           <Grid   
+           container
+            direction="column-reverse"
+            justify="center"
+            alignItems="center"> 
+            <Grid item>
+                <TextField id="standard-basic" label="Email"/>
+            </Grid>
+            <Grid item>
+                <TextField id="standard-basic" label="Senha"/>
+            </Grid>
+
+           </Grid>
+       </FormControl>
+
+    </Container>
     )
 }
 
