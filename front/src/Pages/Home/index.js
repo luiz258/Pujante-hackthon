4f9603ae9131745a3df8c4151c56f93ec631794c
LUIZ MARCELO BARBOSA <luizmarcelo258@gmail.com>
2021-05-23 01:47:29 -0300

    put

--- front/src/Pages/Home/index.js
+++ front/src/Pages/Home/index.js
@@ -2,13 +2,24 @@ import React from "react";
 
 import Navbar from "./../../components/Navbar";
 import Grid from '@material-ui/core/Grid';
-
+import './../Home/index.css'
+import Mapa from "./../../assets/mapaFases.svg";
+import { Link } from "react-router-dom";
+import { Button } from '@material-ui/core';
 function Home(){
     return (
-      <Grid >
+      <>
        <Navbar ></Navbar>
-      
-       </Grid>
+       <Grid container  justify="center" >
+ 
+        <img  src={Mapa} style={{}} />
+        </Grid>
+        <Link className="botao1"><Button  variant="contained" color="primary">Fase 1</Button ></Link>
+        <Link className="botao2"><Button variant="contained" color="primary">Fase 2</Button ></Link>
+        <Link className="botao3"><Button variant="contained" color="primary">Fase 3</Button ></Link>
+     
+</>
+    
     )
 }
 
