db442a0e14b68f88c0a0b728fa9f0e5f09dce4c5
LUIZ MARCELO BARBOSA <luizmarcelo258@gmail.com>
2021-05-22 22:32:51 -0300

    alteracao css

--- front/src/Pages/Narrativa/Empatia/EmpatiaIntroducao/Inicio/index.js
+++ front/src/Pages/Narrativa/Empatia/EmpatiaIntroducao/Inicio/index.js
@@ -11,6 +11,7 @@ import Grid from '@material-ui/core/Grid';
 import Logo from '../../../../../assets/logo.svg';
 import Frame7 from '../../../../../assets/Frame7.svg';
 import '../Inicio/index.css'
+import { Link } from "react-router-dom";
 function Inicio() {
     return (<>
 
@@ -26,11 +27,11 @@ function Inicio() {
                         <img className="center" className="logo" src={Logo} alt="frame" />
                     </Grid>
                     <Grid container className="FabButtonPosistion">
-
+                        <Link to="/narrativa/empatia/introducao/meio">
                         <Fab item xs={12} color="primary" aria-label="arrowRight" variant="extended" >
                             Proximo <KeyboardArrowRightIcon />
                         </Fab>
-
+                        </Link>
                     </Grid>
                 </Grid>
 
